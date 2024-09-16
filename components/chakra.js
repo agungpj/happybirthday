6258dd79f89502b1ca6dffac38967e1aacdfc9f3
Agung Prasetya <agungprasetya1121@gmail.com>
2024-09-16 21:19:42 +0700

    fixing3

--- components/chakra.js
+++ components/chakra.js
@@ -3,6 +3,7 @@ import {
   cookieStorageManagerSSR,
   localStorageManager
 } from '@chakra-ui/react'
+import theme from '../lib/theme'
 
 export default function Chakra({ cookies, children }) {
   const colorModeManager =
@@ -11,7 +12,7 @@ export default function Chakra({ cookies, children }) {
       : localStorageManager
 
   return (
-    <ChakraProvider colorModeManager={colorModeManager}>
+    <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
       {children}
     </ChakraProvider>
   )
